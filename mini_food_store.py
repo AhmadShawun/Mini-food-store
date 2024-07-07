9601b67c0612808ff40d5e51c22da40892976e9e
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-07 19:21:38 +0600

    menu added

---
+++ mini_food_store.py
@@ -0,0 +1,20 @@
+menu = {
+    'Espresso' : 100,
+    'Cappuccino' : 150, 
+    'Latte' : 180 ,
+    'Americano' : 120 ,
+    'Mocha' : 200 ,
+    'Iced Coffee' : 160, 
+    'Green Tea' : 80 ,
+    'Herbal Tea' : 100 ,
+    'Chicken Sandwich' : 250 ,
+    'Beef Burger' : 300 ,
+    'Veggie Wrap' : 200 ,
+    'Caesar Salad' : 220 ,
+    'Grilled Cheese Sandwich' : 180 ,
+    'Pasta Alfredo' : 350 ,
+    'Chicken Wings 6 pieces' : 280 ,
+    'Chocolate Cake Slice' : 150
+}
+
+print(menu)
